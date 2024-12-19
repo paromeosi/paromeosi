b7786b6f86e586008fe87e3de3b25871918e7dc3
Francesco Di Cecca <francescodicecca@AirdiFrancesco.Home>
2024-12-19 20:19:45 +0100

    Add extensive logging and directory permissions check

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,24 +7,10 @@ const fs = require('fs').promises;
 
 const app = express();
 
-// CORS configuration
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://paromeosi.vercel.app',
-  'https://paromeosi.it'
-];
+// CORS configuration semplificata per il debug
+app.use(cors());
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
+// Il resto delle configurazioni
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
